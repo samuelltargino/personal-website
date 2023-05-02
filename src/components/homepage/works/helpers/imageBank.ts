c4370dc2b87cfda5d3dc05ce52e9986b9f23f184
samuelltargino <dev.targino@gmail.com>
2023-05-02 16:31:21 -0300

    :bento: Atualizando as importações dos assets

--- src/components/homepage/works/helpers/imageBank.ts
+++ src/components/homepage/works/helpers/imageBank.ts
@@ -6,38 +6,69 @@ import kingdomsOfDarknessSVG from '../../../../assets/images/projects/svg/kingdo
 import fusionSVG from '../../../../assets/images/projects/svg/fusion.svg';
 import greenVilleSVG from '../../../../assets/images/projects/svg/green-ville.svg'
 
+// WEBP
+
+// Macedo Advogados
+import macedoAdvogadosWEBP_01 from "../../../../assets/images/projects/webp/macedo-advogados_01.webp";
+import macedoAdvogadosWEBP_02 from "../../../../assets/images/projects/webp/macedo-advogados_02.webp";
+import macedoAdvogadosWEBP_03 from "../../../../assets/images/projects/webp/macedo-advogados_03.webp";
+import macedoAdvogadosWEBP_04 from "../../../../assets/images/projects/webp/macedo-advogados_04.webp";
+
+// TaskSpace
+import taskspaceWEBP_01 from "../../../../assets/images/projects/webp/taskspace_01.webp";
+import taskspaceWEBP_02 from "../../../../assets/images/projects/webp/taskspace_02.webp";
+import taskspaceWEBP_03 from "../../../../assets/images/projects/webp/taskspace_03.webp";
+import taskspaceWEBP_04 from "../../../../assets/images/projects/webp/taskspace_04.webp";
+
+// Kingdoms of Darkness
+import kingdomsOfDarknessWEBP_01 from "../../../../assets/images/projects/webp/kingdoms-of-darkness_01.webp";
+import kingdomsOfDarknessWEBP_02 from "../../../../assets/images/projects/webp/kingdoms-of-darkness_02.webp";
+import kingdomsOfDarknessWEBP_03 from "../../../../assets/images/projects/webp/kingdoms-of-darkness_03.webp";
+import kingdomsOfDarknessWEBP_04 from "../../../../assets/images/projects/webp/kingdoms-of-darkness_04.webp";
+
+// Fusion
+import fusionWEBP_01 from "../../../../assets/images/projects/webp/fusion_01.webp";
+import fusionWEBP_02 from "../../../../assets/images/projects/webp/fusion_02.webp";
+import fusionWEBP_03 from "../../../../assets/images/projects/webp/fusion_03.webp";
+import fusionWEBP_04 from "../../../../assets/images/projects/webp/fusion_04.webp";
+
+// green-ville
+import greenVilleWEBP_01 from "../../../../assets/images/projects/webp/green-ville_01.webp";
+import greenVilleWEBP_02 from "../../../../assets/images/projects/webp/green-ville_02.webp";
+import greenVilleWEBP_03 from "../../../../assets/images/projects/webp/green-ville_03.webp";
+import greenVilleWEBP_04 from "../../../../assets/images/projects/webp/green-ville_04.webp";
+
 const WEBP = {
   // Macedo Advogaodos
-  macedoAdvogadosWEBP_01: '/src/assets/images/projects/webp/macedo-advogados_01.webp',
-  macedoAdvogadosWEBP_02: '/src/assets/images/projects/webp/macedo-advogados_02.webp',
-  macedoAdvogadosWEBP_03: '/src/assets/images/projects/webp/macedo-advogados_03.webp',
-  macedoAdvogadosWEBP_04: '/src/assets/images/projects/webp/macedo-advogados_04.webp',
+  macedoAdvogadosWEBP_01: macedoAdvogadosWEBP_01,
+  macedoAdvogadosWEBP_02: macedoAdvogadosWEBP_02,
+  macedoAdvogadosWEBP_03: macedoAdvogadosWEBP_03,
+  macedoAdvogadosWEBP_04: macedoAdvogadosWEBP_04,
 
   // TaskSpace
-  taskspaceWEBP_01: '/src/assets/images/projects/webp/taskspace_01.webp',
-  taskspaceWEBP_02: '/src/assets/images/projects/webp/taskspace_02.webp',
-  taskspaceWEBP_03: '/src/assets/images/projects/webp/taskspace_03.webp',
-  taskspaceWEBP_04: '/src/assets/images/projects/webp/taskspace_04.webp',
+  taskspaceWEBP_01: taskspaceWEBP_01,
+  taskspaceWEBP_02: taskspaceWEBP_02,
+  taskspaceWEBP_03: taskspaceWEBP_03,
+  taskspaceWEBP_04: taskspaceWEBP_04,
 
   // Kingdoms of Darkness
-  kingdomsOfDarknessWEBP_01: '/src/assets/images/projects/webp/kingdoms-of-darkness_01.webp',
-  kingdomsOfDarknessWEBP_02: '/src/assets/images/projects/webp/kingdoms-of-darkness_02.webp',
-  kingdomsOfDarknessWEBP_03: '/src/assets/images/projects/webp/kingdoms-of-darkness_03.webp',
-  kingdomsOfDarknessWEBP_04: '/src/assets/images/projects/webp/kingdoms-of-darkness_04.webp',
+  kingdomsOfDarknessWEBP_01: kingdomsOfDarknessWEBP_01,
+  kingdomsOfDarknessWEBP_02: kingdomsOfDarknessWEBP_02,
+  kingdomsOfDarknessWEBP_03: kingdomsOfDarknessWEBP_03,
+  kingdomsOfDarknessWEBP_04: kingdomsOfDarknessWEBP_04,
 
   // Fusion
-  fusionWEBP_01: '/src/assets/images/projects/webp/fusion_01.webp',
-  fusionWEBP_02: '/src/assets/images/projects/webp/fusion_02.webp',
-  fusionWEBP_03: '/src/assets/images/projects/webp/fusion_03.webp',
-  fusionWEBP_04: '/src/assets/images/projects/webp/fusion_04.webp',
+  fusionWEBP_01: fusionWEBP_01,
+  fusionWEBP_02: fusionWEBP_02,
+  fusionWEBP_03: fusionWEBP_03,
+  fusionWEBP_04: fusionWEBP_04,
 
   // green-ville
-  greenVilleWEBP_01: '/src/assets/images/projects/webp/green-ville_01.webp',
-  greenVilleWEBP_02: '/src/assets/images/projects/webp/green-ville_02.webp',
-  greenVilleWEBP_03: '/src/assets/images/projects/webp/green-ville_03.webp',
-  greenVilleWEBP_04: '/src/assets/images/projects/webp/green-ville_04.webp',
+  greenVilleWEBP_01: greenVilleWEBP_01,
+  greenVilleWEBP_02: greenVilleWEBP_02,
+  greenVilleWEBP_03: greenVilleWEBP_03,
+  greenVilleWEBP_04: greenVilleWEBP_04,
 };
-
 const SVG = {
   macedoAdvogadosSVG: macedoAdvogadosSVG,
   taskSpaceSVG: taskSpaceSVG,
